969537871fd29ab9c9254ead1a395e45cb8de739
Abhie-Tripathi <abhiiii.tripathi@gmail.com>
2023-08-04 00:10:38 +0530

    Displaying the fetched data in table

---
+++ src/components/Analytics.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Analytics = () => {
+  return (
+    <div className='flex flex-col'>
+        <div className="font-semibold h-screen w-20 bg-[#192F48]">hii</div>
+
+    </div>
+  )
+}
+
+export default Analytics
